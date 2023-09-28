f61fef466d0eebe657c100de7425fe0eb95d7c70
Ryan Manchester <ryanm17@gmail.com>
2023-09-28 14:21:26 -0400

    adds styled table on homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './layout/NavBar'
-
+import Home from './pages/Home'
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
     </div>
   );
 }
